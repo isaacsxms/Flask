7d3dd7d466649d4afc049a10b42c67345bb0e735
isaacsxms <isaacsxms@gmail.com>
2024-05-13 18:51:54 +0200

    new additions

--- models.py
+++ models.py
@@ -2,6 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Post(db.Model):
+#class Post(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    title = db.Column(db.String(80), unique=True, nullable=False)
+
+class Albaran(db.Model):
+    __tablename__ = 'albaran'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True, nullable=False)
+    proveedor = db.Column(db.String(50), nullable=False)
+    producto = db.Column(db.String(50), nullable=False)
+    removed = db.Column(db.Boolean, nullable=False) # Querys only to removed=False, if removed it should not be query'd
+
+class Producto(db.Model):
+    __tablename__ = 'producto'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), unique=True, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+
+class Linea_Producto_Albaran(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_albaran = db.Column(db.Integer, db.ForeignKey('albaran.id'))
+    id_producto = db.Column(db.Integer, db.ForeignKey('producto.id'))
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, url_for
-from models import Post, db
+from models import  Producto, db #Post
 from forms import PostForm
 
 main = Blueprint('main', __name__)
@@ -12,23 +12,33 @@ def home():
 def adios():
     return "adios"
 
-@main.route("/posts")
-def posts():
-    posts = Post.query.all()
-    return render_template("posts.html", posts=posts)
+#@main.route("/posts")
+#def posts():
+#    posts = Post.query.all()
+#    return render_template("posts.html", posts=posts)
 
-@main.route("/post/<int:post_id>")
-def post(post_id):
-    post = Post.query.get_or_404(post_id)
-    return render_template("post.html", post=post)
+#@main.route("/post/<int:post_id>")
+#def post(post_id):
+#    post = Post.query.get_or_404(post_id)
+#    return render_template("post.html", post=post)
 
-@main.route("/createpost", methods=["GET", "POST"])
-def add_post():
-    form = PostForm()
-    if form.validate_on_submit():
-        title = request.form.get("title")
-        post = Post(title=title)
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for("main.home"))
-    return render_template("create_post.html", form=form)
+#@main.route("/createpost", methods=["GET", "POST"])
+#def add_post():
+#    form = PostForm()
+#    if form.validate_on_submit():
+#       title = request.form.get("title")
+#        post = Post(title=title)
+#        db.session.add(post)
+#        db.session.commit()
+#        return redirect(url_for("main.home"))
+#    return render_template("create_post.html", form=form)
+
+@main.route("/producto/<int:producto_id>")
+def producto(producto_id):
+    producto = Producto.query.get_or_404(producto_id)
+    return render_template("producto.html", producto=producto)
+
+@main.route("/productos") # por defecto es GET
+def productos():
+    productos = Producto.query.all()
+    return render_template("productos.html", productos=productos)
